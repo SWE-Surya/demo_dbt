60241641d3eae9e13d8d498b339e3c151122d7c5
SWE-Surya <141406782+SWE-Surya@users.noreply.github.com>
2024-05-05 00:26:01 +0530

    Update test_tbl.sql

--- models/example/test_tbl.sql
+++ models/example/test_tbl.sql
@@ -2,8 +2,6 @@
 {{ config(materialized='table') }}
 
 
-with new_source_data as (
-  
 SELECT
     ID,
     FIRST_NAME,
@@ -14,6 +12,3 @@ SELECT
 	CONCAT(FIRST_NAME,LAST_NAME) AS FULL_NAME
 FROM FIVETRAN_DATABASE.GOOGLE_SHEETS.EMPLOYEES
 
-)
-
-select * from new_source_data
